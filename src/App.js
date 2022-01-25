b8a01983185c74db7ba56bf31596aca29b5ca589
SnehaMane17 <snehamane171998@gmail.com>
2022-01-25 11:37:39 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+import photo from "./DSC01014.JPG";
+
+function Header(props){
+ 
+  return(
+    <header>
+      <h1>{props.name}</h1>
+    </header>
+    
+  );
+}
+function Main(props){
+  return(
+    <main>
+      <h2>She is a React developer</h2>
+      <ul>
+        {props.foods.map((food,i) => <li key={i}>{food}</li>)}
+
+      </ul>
+      
+    </main>
+    
+  );
+}
+function Footer(){
+  return(
+    <footer>
+      <h3>She works at birlasoft</h3>
+      <img src={photo} height={200}></img>
+    </footer>
+    
+  );
+}
+const foods = [
+  "Onbase",
+  ".net",
+  "C#"
+];
+
+function App() {
+  return (
+    <div className="App">
+      <Header name="Sneha" />
+      <Main foods={foods}/>
+      <Footer/>
+   
+    </div>
+  );
+}
+
+export default App;
